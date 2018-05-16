bdebf150c376e0b832253ad3e5f33e4f27c7e72a
Sam Hamra <samhamrasnya@gmail.com>
2018-05-16 14:28:34 +0200

    final fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+jQuery('html,body').animate({scrollTop:0},0);
 // Create an empty sensor object as a global
 var office = {};
 var recreational = {}
@@ -25,10 +25,11 @@ $(window).resize(function(){
 
 
 function drawChart(recreationalData, officeData, index) {
+  console.log("DRAWCHART")
       if(index< 0) {
         return;
       }
-			 console.log("DRAWCHART")
+
 			 var variable = varNames[index];
 			 var header = headers[index];
 			 var lines = [];
@@ -54,7 +55,28 @@ function drawChart(recreationalData, officeData, index) {
 		 }
 
 // Function to retrieve data, placing it in a "response" object
-function getJSON(key, area) {
+function getJSON(key, area)
+    {
+    if (window.cordova)
+        {
+            console.log('Using Apache Cordova HTTP GET function');
+            cordovaHTTP.get(
+                sensorURL + key + '.json?gt[timestamp]=now-1day&page=1',
+                function (response)
+                    {
+                        if (response)
+                            {
+                                area.data = response;
+                            }
+                    },
+                function (error)
+                    {
+                    console.log(JSON.stringify(error));
+                    });
+        }
+    else
+        {
+            console.log('Not using Cordova, fallback to AJAX via jquery');
             $.ajax({
                     url: sensorURL + key + ".json?gt[timestamp]=now-1day",
                     jsonp: "callback",
@@ -65,14 +87,39 @@ function getJSON(key, area) {
                     },
                     success: function(response)
                         {
+                          console.log("success");
                             if (response && response[0])
                                 {
-
+                                  console.log(response);
 																	    area.data = response;
+                                      if(recreational.data && office.data) {
+                                        toggleButtons();
+                                      }
 
                                 }
-                        }
+                        },
+                  error: function(error)
+                  {
+                    console.log(error);
+                  }
                 });
+        }
+}
+
+function toggleButtons() {
+  $(".dataButton").show();
+
+}
+
+function scrollToGraph() {
+
+	var $window = $(window),
+		$element = $('#chart_div'),
+		elementTop = $element.offset().top,
+		elementHeight = $element.height(),
+		viewportHeight = $window.height(),
+		scrollIt = elementTop - ((viewportHeight - elementHeight) / 2);
+	$window.scrollTop(scrollIt);
 }
 
 function getData(index) {
@@ -80,6 +127,7 @@ function getData(index) {
 	google.charts.load('current', {'packages':['annotationchart']});
 	google.charts.setOnLoadCallback(function () {
 		console.log("getData")
-	  drawChart(recreational.data, office.data, currentgraph);
+	  drawChart(recreational.data, office.data, index);
+    scrollToGraph();
 	});
 }
